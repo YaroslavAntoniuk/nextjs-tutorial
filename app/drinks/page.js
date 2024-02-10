4883497731de8b50ab4f3bef4b8d5e7d4d5aa68f
YaroslavAntoniuk <i23-1912-701-01@lute.lviv.ua>
2024-02-10 14:36:31 +0200

    Added basic drinks page with fetching & styling

--- app/drinks/page.js
+++ app/drinks/page.js
@@ -1,7 +1,37 @@
-const Drinks = () => {
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail';
+
+const fetchDrinks = async () => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    return data.drinks;
+  } catch (error) {
+    throw new Error('Something went wrong when fetching drinks');
+  }
+};
+
+const Drinks = async () => {
+  const drinks = await fetchDrinks();
+
   return (
-    <div>
-      <h1 className="text-7xl">Drinks</h1>
+    <div className="h-full">
+      <h1 className="text-7xl mb-4">Drinks</h1>
+      <div className="flex flex-wrap justify-center h-full overflow-scroll no-scrollbar relative">
+        {drinks.map((drink) => (
+          <div
+            key={drink.idDrink}
+            className="flex flex-col items-center justify-center p-8 w-60 m-2 rounded-xl bg-neutral-500"
+          >
+            <h2 className="text-white mb-4">{drink.strDrink}</h2>
+            <img
+              src={drink.strDrinkThumb}
+              alt={drink.strDrink}
+              className="w-32 h-32 rounded-lg"
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
