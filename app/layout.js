5e876946295efbf32fcd76aa047b428309088d88
YaroslavAntoniuk <i23-1912-701-01@lute.lviv.ua>
2024-02-10 14:37:03 +0200

    Changed root layout

--- app/layout.js
+++ app/layout.js
@@ -13,8 +13,15 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        <main className="px-8 py-20 max-w-6xl mx-auto">{children}</main>
+        <div className="h-screen">
+          <div className="h-1/6">
+            <Navbar />
+          </div>
+
+          <main className="h-[70%] relative px-8 py-16 max-w-6xl mx-auto">
+            {children}
+          </main>
+        </div>
       </body>
     </html>
   );
