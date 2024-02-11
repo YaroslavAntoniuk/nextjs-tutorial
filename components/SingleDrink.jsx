59c2ff9da3fa8b9c666b16e2d20e63afd9359053
YaroslavAntoniuk <i23-1912-701-01@lute.lviv.ua>
2024-02-11 14:24:14 +0200

    Updated image tag with nextjs Image

--- components/SingleDrink.jsx
+++ components/SingleDrink.jsx
@@ -1,10 +1,16 @@
+import Image from 'next/image';
+
 const SingleDrink = ({ drink }) => {
   return (
     <div className="flex flex-col items-center justify-center p-8 w-60 m-2 rounded-xl h-[275px] bg-neutral-500/25">
       <h2 className="text-white mb-4">{drink.strDrink}</h2>
-      <img
+      <Image
+        id={drink.idDrink}
         src={drink.strDrinkThumb}
         alt={drink.strDrink}
+        width={128}
+        height={128}
+        priority
         className="w-32 h-32 rounded-lg"
       />
     </div>
