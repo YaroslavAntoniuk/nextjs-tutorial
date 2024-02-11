04487c0e064a05a50f09a0f347333ba5306026b6
YaroslavAntoniuk <i23-1912-701-01@lute.lviv.ua>
2024-02-11 12:52:02 +0200

    Fixed layouts styling

--- app/drinks/layout.js
+++ app/drinks/layout.js
@@ -1,6 +1,6 @@
 const DrinksLayout = ({ children }) => {
   return (
-    <div className="max-w-xl">
+    <div className="h-full">
       <div className="mockup-code mb-8">
         <pre data-prefix="$">
           <code>npx create-next-app@latest nextjs-tutorial</code>
--- app/layout.js
+++ app/layout.js
@@ -18,7 +18,7 @@ export default function RootLayout({ children }) {
             <Navbar />
           </div>
 
-          <main className="h-[70%] relative px-8 py-16 max-w-6xl mx-auto">
+          <main className="h-full relative px-8 py-8 max-w-6xl mx-auto">
             {children}
           </main>
         </div>
