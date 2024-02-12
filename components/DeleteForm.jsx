b990b6450c6ce76b1cc1f45b8cd7b90cc564dc93
YaroslavAntoniuk <i23-1912-701-01@lute.lviv.ua>
2024-02-13 00:05:54 +0200

    Added delete task button

--- components/DeleteForm.jsx
+++ components/DeleteForm.jsx
@@ -1,10 +1,27 @@
+'use client';
+
 import { deleteTask } from '@/utils/actions';
+import { useFormStatus } from 'react-dom';
+
+const DeleteButton = () => {
+  const { pending } = useFormStatus();
+
+  return (
+    <button
+      type="submit"
+      className="btn btn-error btn-xs px-4 text-slate-50"
+      disabled={pending}
+    >
+      {pending ? 'Please wait...' : 'Delete Task'}
+    </button>
+  );
+};
 
 const DeleteForm = ({ taskId }) => {
   return (
     <form action={deleteTask}>
       <input type="hidden" name="id" value={taskId} />
-      <button className="btn btn-xs btn-error">Delete</button>
+      <DeleteButton />
     </form>
   );
 };
